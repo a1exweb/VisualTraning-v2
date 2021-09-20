ce9810a8ca97a853b3d731f6e29a872626f2e4d7
a1exweb <a1exweb@mail.ru>
2021-09-20 12:29:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,17 +2,28 @@ function changeBg() {
   document.querySelector('body').classList.toggle('active');
 };
 
-document.addEventListener('keyup', () => {
-  changeBg();  
-});
-
-document.querySelector('body').addEventListener('mousedown', (e) => {
-  if (!e.target.matches('p') && !e.target.matches('#theme')) {
-    changeBg();
+document.addEventListener('keyup', (e) => {
+   if (e.keyCode == 38) { //стрелочка ввехрх
+   changeBg(); 
   }
 });
 
-document.querySelector('#theme').addEventListener('click', () => {
+theme.addEventListener('click', () => {
   document.querySelector('body').classList.toggle('theme-white');
   document.querySelector('body').classList.toggle('theme-dark');
 });
+
+traning.addEventListener('click', () => {
+  changeBg();
+});
+
+setTimeout
+
+const loader = document.querySelector('.description')
+
+setTimeout(() => {
+  const interval = setInterval(() => {
+  loader.style.opacity -= 0.01;
+  if (loader.style.opacity == 0) clearInterval(interval);
+}, 10);
+}, 1500);
